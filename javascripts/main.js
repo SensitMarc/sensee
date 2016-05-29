615ace7195a78a2fed55bf4dcf6b9b1a6bf8b2bd
SensitMarc <sensitlabs@gmail.com>
2016-05-29 15:40:21 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,5 +4,42 @@ var total = price1 + price2;
 document.getElementById("demo").innerHTML =
 "The total is: " + total;
 
+var client = new Keen({
+  projectId: "5368fa5436bf5a5623000000",
+  readKey: "3f324dcb5636316d6865ab0ebbbbc725224c7f8f3e8899c7733439965d6d4a2c7f13bf7765458790bd50ec76b4361687f51cf626314585dc246bb51aeb455c0a1dd6ce77a993d9c953c5fc554d1d3530ca5d17bdc6d1333ef3d8146a990c79435bb2c7d936f259a22647a75407921056"
+});
+Keen.ready(function(){
+  // ----------------------------------------
+  // Sample one
+  // ----------------------------------------
+  var pageviews_timeline = new Keen.Query("count", {
+    eventCollection: "pageviews",
+    interval: "hourly",
+    groupBy: "user.device_info.browser.family",
+    timeframe: {
+      start: "2014-05-04T00:00:00.000Z",
+      end: "2014-05-05T00:00:00.000Z"
+    }
+  });
+  client.draw(pageviews_timeline, document.getElementById("chart-01"), {
+    chartType: "areachart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "80%"
+      },
+      isStacked: true
+    }
+  });
+  // ----------------------------------------
+  //  End sample one
+  // ----------------------------------------
+
+
 
 
