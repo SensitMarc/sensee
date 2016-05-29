bbb5209579c57b01c4d39015f840c44addd2ecd2
SensitMarc <sensitlabs@gmail.com>
2016-05-29 15:27:46 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,8 @@
-console.log('This would be the main JS file.');
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+document.getElementById("demo").innerHTML =
+"The total is: " + total;
 
 
 
